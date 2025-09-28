901a30fd7b680f2298c6bf8a82f610045f0682cf
Adarshagupta <adarshagupta@users.noreply.github.com>
2025-09-28 11:05:58 +0530

    remoe useless files

--- mock-internal-bytecode.js
+++
@@ -1,8 +0,0 @@
-// Mock InternalBytecode.js to prevent Metro bundler errors
-// This file is used as a fallback when the real InternalBytecode.js is not found
-
-module.exports = {
-  // Mock implementation to prevent build errors
-  default: () => {},
-  __esModule: true
-};
